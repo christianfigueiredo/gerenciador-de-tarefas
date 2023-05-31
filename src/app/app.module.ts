d26a16d25dd1c951a51caab860059779bdd1e124
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-05-31 16:18:42 -0300

    criação do model tarefa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarefasModule } from './tarefas';
 
+
 @NgModule({
   declarations: [
     AppComponent
